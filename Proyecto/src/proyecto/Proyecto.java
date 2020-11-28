682f4e75b27a38917854f4b3a8bc4810eeab84ea
MIKEcsl5 <soccerpoof013@gmail.com>
2020-11-27 20:40:06 -0600

    Prueba

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -16,6 +16,7 @@ public class Proyecto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Prueba jsjsjs XD");
     }
     
 }
