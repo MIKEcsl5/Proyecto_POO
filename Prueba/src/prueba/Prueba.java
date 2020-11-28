34cf71dda0a59d1476da0e720601179ec7607922
Daniel Rojas <57378034+EclipseDR3@users.noreply.github.com>
2020-11-27 23:48:13 -0600

    Update Prueba.java

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -17,6 +17,7 @@ public class Prueba {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("PRUEBA GIT");
+        System.out.println("Otra pureba de Daniel Rojas");
     }
     
 }
