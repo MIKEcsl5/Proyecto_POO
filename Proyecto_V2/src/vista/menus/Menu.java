6acb617d33be561673355d106fd572188b09703f
Daniel Rojas <57378034+EclipseDR3@users.noreply.github.com>
2021-01-11 23:54:22 -0600

    Agregado de clases para el paquete menus

---
+++ Proyecto_V2/src/vista/menus/Menu.java
@@ -0,0 +1,20 @@
+
+package vista.menus;
+
+/**
+ * Esta clase supone un menú generico de entrada al usuario donde se presenta el acceso de un empleado,
+ * este deberá identificarse para acceder a ciertas características según su nivel de usuario.
+ * @author Daniel Rojas
+ */
+public abstract class Menu {
+    
+    public int respuesta;
+
+    /**
+     * El método despliegue es un método abstracto que debe ser implementado por cada menu específico
+     * de el nivel de empleado (gerente, vendedor o acomodador) que se obtiene del
+     * paquete "vista.caja"; su implementación supone mostrar las acciones disponibles del empleado.
+     */
+    public abstract void despliegue();
+    
+}
---
+++ Proyecto_V2/src/vista/menus/MenuAcomodador.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista.menus;
+
+/**
+ *
+ * @author Daniel Rojas
+ */
+public class MenuAcomodador extends Menu{
+    
+    /**
+     * Este método despliega el menú específico de un usuario que accedió como acomodador.
+     */
+    @Override
+    public void despliegue(){
+        
+    }
+}
